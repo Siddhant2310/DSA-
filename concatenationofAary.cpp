01590dac6647b9148062855b1721f545668a63a4
Siddhant_184 <siddhantmishrafzd@gmail.com>
2026-09-16 20:15:48 +0530

    Update array solutions

--- concatenationofAary.cpp
+++ concatenationofAary.cpp
@@ -1,5 +1,7 @@
 
-
+#include <iostream>
+#include <vector>
+using namespace std;
 // class Solution {
 // public:
 //     vector<int> getConcatenation(vector<int>& nums) {
--- shuffleArray.cpp
+++ shuffleArray.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
