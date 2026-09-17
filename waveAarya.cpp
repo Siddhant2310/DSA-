3f38116478aa3f7a14b2d3206a84d1145cfda4ab
Siddhant_184 <siddhantmishrafzd@gmail.com>
2026-09-17 23:11:11 +0530

    Add wave array solution

---
+++ waveAarya.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;class Solution {
+  public:
+    void sortInWave(vector<int>& arr) {
+        for(int i=0;i<arr.size()-1;i+=2){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+};
